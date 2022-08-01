81eac96cacf697f74e20faae1e4c08ec21588c64
Nyasha <nmawungwe@gmail.com>
2022-08-01 17:38:14 +0200

    added metadatURL

--- NFT-Collection/hardhat-tutorial/constants/index.js
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of whitelist contract deployed 
 const WHITELIST_CONTRACT_ADDRESS="0x5FbDB2315678afecb367f032d93F642f64180aa3";
 // URL to extract Metadata for a Crypto Dev NFT 
-const METADATA_URL="https://web3-baqfji0oz-nmawungwe.vercel.app/api/";
+const METADATA_URL="https://web3-ck83z5mvm-nmawungwe.vercel.app/api/";
 
 module.exports = {WHITELIST_CONTRACT_ADDRESS, METADATA_URL};
