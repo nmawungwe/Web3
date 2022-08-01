6c25b649e433e7cc9d37aca84ef03dac74f59360
Nyasha <nmawungwe@gmail.com>
2022-08-01 17:13:00 +0200

    Developed front for minting website and API for NFT metadata

---
+++ NFT-Collection/my-app/pages/api/[tokenId].js
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// Base URI + TokenID
+// Base URI = https://example.com/ 
+// Token ID = 1 
+// tokenURI(1) => https://example.com/1
+
+export default function handler(req, res) {
+ 
+    const tokenId = req.query.tokenId;
+
+    const name = `Crypto Dev #${tokenId}`;
+    const description = "CryptoDevs is an NFT collection for Web3 developers. LFG.";
+    const image = `https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/${
+                    Number(tokenId - 1)
+                    }.svg`;
+
+    return res.json({
+        name: name,
+        description: description,
+        image: image
+    });
+    
+}
