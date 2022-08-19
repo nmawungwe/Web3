1ea6a2d64ce82168e0efaaa97201e41b7796a4bd
Nyasha <nmawungwe@gmail.com>
2022-08-19 11:42:50 +0200

    Completed DEFI exchange SmartContract

--- DefiExchange/hardhat-tutorial/contracts/Exchange.sol
+++ DefiExchange/hardhat-tutorial/contracts/Exchange.sol
@@ -23,7 +23,7 @@ contract Exchange is ERC20 {
      /**
         @dev Adds liquidity to the exchange
       */
-    function addLiquidity(uint _amount) public payable returns (uint) {
+    function addLiquidity(uint _amount) public payable returns (uint liquidityTokens) {
         uint liquidity;
         uint ethBalance = address(this).balance;
         uint cryptoDevTokenReserve = getReserve();
@@ -130,5 +130,50 @@ contract Exchange is ERC20 {
         return numerator / denominator;
     }
 
+    /** 
+    * @dev Swaps Eth for CryptoDev Tokens
+    */
+    function ethToCryptoDevToken(uint _minTokens) public payable {
+        uint256 tokenReserve = getReserve();
+        // call the `getAmountOfTokens` to get the amount of Crypto Dev tokens
+        // that would be returned to the user after the swap
+        // Notice that the `inputReserve` we are sending is equal to  
+        // `address(this).balance - msg.value` instead of just `address(this).balance`
+        // because `address(this).balance` already contains the `msg.value` user has sent in the given call
+        // so we need to subtract it to get the actual input reserve
+        uint256 tokensBought = getAmountOfTokens(
+            msg.value, 
+            address(this).balance - msg.value, 
+            tokenReserve
+            );
+        
+        require(tokensBought >= _minTokens, "insufficient output amount");
+        // Transfer the `Crypto Dev` tokens to the user
+        ERC20(cryptoDevTokenAddress).transfer(msg.sender, tokensBought);        
+    }
 
+    /** 
+    * @dev Swaps CryptoDev Tokens for Eth
+    */
+    function cryptoDevTokenToEth(uint _tokensSold, uint _minEth) public {
+        uint256 tokenReserve = getReserve();
+        // call the `getAmountOfTokens` to get the amount of Eth
+        // that would be returned to the user after the swap
+        uint256 ethBought = getAmountOfTokens(
+            _tokensSold,
+            tokenReserve,
+            address(this).balance
+        );
+
+        require(ethBought >= _minEth, "insufficient output amount");
+        
+        // Transfer `Crypto Dev` tokens from the user's address to the contract
+        ERC20(cryptoDevTokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            _tokensSold
+        );
+        // send the `ethBought` to the user from the contract
+        payable(msg.sender).transfer(ethBought);
+    }
 }
